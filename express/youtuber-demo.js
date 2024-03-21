5f5401fa4013602901932cf90b244441c026d7d8
Eunchae Kim <kec9706@gmail.com>
2024-03-21 22:40:10 +0900

    add delete, put feature

--- express/youtuber-demo.js
+++ express/youtuber-demo.js
@@ -30,11 +30,14 @@ db.set(id++, youtuber1);
 db.set(id++, youtuber2);
 db.set(id++, youtuber3);
 
-// controller
+/* 
+controller
+*/
 app.get("/youtubers", function (req, res) {
   res.json(Object.fromEntries(db));
 });
-app.get("/youtuber/:id", function (req, res) {
+
+app.get("/youtubers/:id", function (req, res) {
   let { id } = req.params;
   id = parseInt(id);
 
@@ -47,9 +50,54 @@ app.get("/youtuber/:id", function (req, res) {
     res.json(youtuber);
   }
 });
-app.post("/youtuber", (req, res) => {
+
+app.post("/youtubers", (req, res) => {
   db.set(id++, req.body);
   res.json({
     message: `${db.get(id - 1).channelTitle}님이 유튜버로 추가되었습니다.`,
   });
+});
+
+app.delete("/youtubers/:id", function (req, res) {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  const youtuber = db.get(id);
+  if (youtuber) {
+    db.delete(id);
+    res.json({
+      message: `${youtuber.channelTitle}님의 채널이 삭제되었습니다.`,
+    });
+  } else {
+    res.json({ message: `${id}번의 채널이 존재하지 않습니다.` });
+  }
+});
+
+app.delete("/youtubers", function (req, res) {
+  let message;
+  if (db.size >= 1) {
+    db.clear();
+    message = "전체 유튜버가 삭제되었습니다.";
+  } else {
+    message = "삭제할 유튜버가 없습니다.";
+  }
+
+  res.json({ message });
+});
+
+app.put("/youtubers/:id", function (req, res) {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  const youtuber = db.get(id);
+  if (youtuber) {
+    youtuber.channelTitle = req.body.channelTitle;
+    db.set(id, youtuber);
+
+    res.json({
+      message: `채널명이 ${youtuber.channelTitle}로 변경되었습니다.`,
+    });
+  } else {
+    res.json({ message: `${id}번의 채널이 존재하지 않습니다.` });
+  }
 });
