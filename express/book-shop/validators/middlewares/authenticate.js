03fb533641f063f451f816f8cfcc1add97594c4a
Eunchae Kim <kec9706@gmail.com>
2024-04-22 16:55:32 +0900

    jwt 인증시 에러 타입별로 예외처리 추가

--- express/book-shop/validators/middlewares/authenticate.js
+++ express/book-shop/validators/middlewares/authenticate.js
@@ -16,10 +16,21 @@ const authenticate = (req, res, next) => {
     req.userId = decodedJwt.id;
     next();
   } catch (error) {
-    // TODO: 기간 만기, 토큰 오류 등 에러에 맞춰서 코드, 메세지 반환하기
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ message: "토큰이 유효하지 않습니다." });
+    if (error instanceof jwt.TokenExpiredError) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "토큰이 만기되었습니다." });
+    }
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "유효하지 않은 토큰입니다." });
+    }
+    if (error instanceof jwt.NotBeforeError) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "토큰이 비활성화 상태입니다." });
+    }
   }
 };
 
