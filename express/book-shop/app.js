df775d2186528eb8b53aa2cd50777eda69e5b904
Eunchae Kim <kec9706@gmail.com>
2024-06-08 03:28:56 +0900

    안쓰이는 likes route 제거

--- express/book-shop/app.js
+++ express/book-shop/app.js
@@ -8,7 +8,6 @@ import express from "express";
 import convertSnakeToCamelResponse from "./middlewares/convertSnakeToCamelResponse.js";
 import booksRouter from "./routes/books.js";
 import cartsRouter from "./routes/carts.js";
-import likesRouter from "./routes/likes.js";
 import ordersRouter from "./routes/orders.js";
 import usersRouter from "./routes/users.js";
 
@@ -22,4 +21,3 @@ app.use("/", usersRouter);
 app.use("/books", booksRouter);
 app.use("/carts", cartsRouter);
 app.use("/orders", ordersRouter);
-app.use("/likes", likesRouter);
